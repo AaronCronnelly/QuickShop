aa14e56eb8ba962885f28d31f82747da731a6419
SOPHIE BOYLE - STUDENT <g00410444@atu.ie>
2024-02-28 13:14:00 +0000

    added services section and styled, other slight adjustments

--- QuickShop-main/quick-shop/src/Navbar.js
+++ QuickShop-main/quick-shop/src/Navbar.js
@@ -1,19 +1,18 @@
 // Navbar.js
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from './assets/images/logo.png'; // Ensure the path to the logo is correct
+import logo from './assets/images/logo.png'; 
 
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      {/* The logo image is now placed inside an img tag and wrapped with a div */}
       <div className="navbar-brand">
         <img src={logo} alt="QuickShop Logo" className="navbar-logo" />
       </div>
       <div className="navbar-links">
         <NavLink to="/" exact activeClassName="navbar-active">Home</NavLink>
-        <NavLink to="/about" activeClassName="navbar-active">About</NavLink>
+        <NavLink to="#about-section" activeClassName="navbar-active">About</NavLink>
         <NavLink to="/login" activeClassName="navbar-active">Login</NavLink>
         <NavLink to="/services" activeClassName="navbar-active">Services</NavLink>
         {/* Make sure your route paths are correctly defined */}
--- QuickShop-main/quick-shop/src/Services.js
+++ QuickShop-main/quick-shop/src/Services.js
@@ -1,8 +1,45 @@
-// Login.js
 import React from 'react';
+import './App'; 
+
+import service1 from './assets/images/service1.jpg';
+import service2 from './assets/images/service2.jpg';
+import service3 from './assets/images/service3.jpg';
+
+const ServiceCard = ({ image, title, description, buttonText }) => (
+  <div className="service-card">
+    <img src={image} alt={title} className="service-image" />
+    <h3>{title}</h3>
+    <p className="service-description">{description}</p>
+    <button className="btn-book">{buttonText}</button>
+  </div>
+);
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="services-container">
+      <h2 className="services">Services</h2>
+      <div className="services-grid">
+        <ServiceCard
+          image={service1}
+          title="Shopping List"
+          description="Click here to begin making your shopping list"
+          buttonText="Start List"
+        />
+        <ServiceCard
+          image={service2}
+          title="Check Map"
+          description="Check your detailed map here of your local supermarket"
+          buttonText="View Map"
+        />
+        <ServiceCard
+          image={service3}
+          title="Log In"
+          description="Login or register here to check your profile and previous lists"
+          buttonText="Sign In"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Services;
