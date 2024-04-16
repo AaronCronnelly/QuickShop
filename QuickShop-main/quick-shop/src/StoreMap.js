19bcb45abc1034f107f343d70f0f7b1a30b98b55
sophieboyle1 <sophieboylemiami@gmail.com>
2024-04-16 12:43:05 +0100

    completed coordinates of all categories of items on the store map

--- QuickShop-main/quick-shop/src/StoreMap.js
+++ QuickShop-main/quick-shop/src/StoreMap.js
@@ -2,6 +2,7 @@ import React from 'react';
 import map from './assets/images/map.png';
 import mapGrid from './assets/images/mapGrid.png';
 
+// Fixed graph structure
 const graph = {
   entrance: {
     coordinates: { x: 30, y: 225 },
@@ -10,50 +11,49 @@ const graph = {
     },
   },
   aisle1_start: {
-    coordinates: { x: 2, y: 1 },
+    coordinates: { x: 60, y: 225 },
     adjacent: {
       entrance: 1,
       aisle1_end: 1,
     },
   },
   aisle1_end: {
-    coordinates: { x: 2, y: 2 },
+    coordinates: { x: 60, y: 250 },
     adjacent: {
       aisle1_start: 1,
-      aisle2_start: 1,
     },
   },
-  aisle2_start: {
-    coordinates: { x: 3, y: 2 },
+  exit: {
+    coordinates: { x: 320, y: 80 },
     adjacent: {
-      aisle1_end: 1,
-      aisle2_end: 1,
     },
   },
 };
 
-
 const StoreMap = ({ selectedShop, items }) => {
   const allItems = [
     ...items,  // spreads the existing items passed as props
-  { name: 'register', x: 320, y: 400 },
-  { name: 'snacks', x: 368, y: 509 },
-  { name: 'pasta', x: 220, y: 290 },
-  { name: 'bakery', x: 360, y: 230 },
-  { name: 'dairy', x: 75, y: 220 },
   { name: 'entrance', x: 30, y: 225 },
   { name: 'exit', x: 320, y: 80 },
-  /*{ name: 'frozen foods', x: 620, y: 350 },
-  { name: 'canned goods', x: 368, y: 509 },
-  { name: 'beverages', x: 550, y: 300 },
-  { name: 'canned goods', x: 470, y: 450 },
-  { name: 'cereal', x: 500, y: 320 },
-  { name: 'pasta', x: 410, y: 390 },
-  { name: 'spices', x: 380, y: 320 },
-  { name: 'condiments', x: 330, y: 340 },
-  { name: 'health & beauty', x: 200, y: 250 },
-  { name: 'household supplies', x: 180, y: 300 },
-  { name: 'pet supplies', x: 520, y: 220 },*/
+  { name: 'register', x: 320, y: 400 },
+  { name: 'snacks', x: 368, y: 480 },
+  { name: 'pasta', x: 220, y: 290 },
+  { name: 'fruit', x: 70, y: 320 },
+  { name: 'vegetables', x: 170, y: 380 },
+  { name: 'sauces', x: 280, y: 330 },
+  { name: 'bakery', x: 360, y: 230 },
+  { name: 'dairy', x: 75, y: 220 },
+  { name: 'meat', x: 150, y: 160 },
+  { name: 'fish', x: 240, y: 110 },
+  { name: 'cereal', x: 250, y: 260 },
+  { name: 'spices', x: 240, y: 360 },
+  { name: 'condiments', x: 310, y: 300 },
+  { name: 'frozen foods', x: 430, y: 400 },
+  { name: 'health & beauty', x: 430, y: 160 },
+  { name: 'household supplies', x: 500, y: 200 },
+  { name: 'cans', x: 350, y: 280 },
+  { name: 'beverages', x: 500, y: 300 },
+  { name: 'pet supplies', x: 560, y: 245 }
   ];
 
   return (
